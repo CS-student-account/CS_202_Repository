e1b3f0275080d284a19b2ef059385958e614136b
CS-student-account <dmisyura@alaska.edu>
2022-01-20 13:49:11 -0900

    Created Temperature.cpp

---
+++ HW 0/Temperature/Temperature.cpp
@@ -0,0 +1,18 @@
+/*
+* Temperature.cpp
+* David Misyura
+* 20 Jan 2021
+* HW 0 Temperature.cpp for CS 202
+*/
+
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setprecision;
+
+int main()
+{
+	return 0;
+}
