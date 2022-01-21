df07d5cf3594b08061ccfe18df128fa5ded0014a
CS-student-account <dmisyura@alaska.edu>
2022-01-20 21:20:41 -0900

    Created excerptinator.cpp.

---
+++ HW 0/excerptinator/excerptinator.cpp
@@ -0,0 +1,19 @@
+/*
+* excerptinator.cpp
+* David Misyura
+* 20 Jan 2021
+* HW 0 excerptinator.cpp for CS 202
+*/
+
+#include <string>
+#include <iostream>
+#include <fstream>
+using std::endl;
+using std::string;
+using std::cout;
+
+int main()
+{
+
+    return 0;
+}
