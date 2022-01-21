d1390951dde2e99d2ecc4c933eac5233c2c6031e
CS-student-account <dmisyura@alaska.edu>
2022-01-20 21:09:14 -0900

    Fully commented everything in temperature.cpp.

--- HW 0/Temperature/Temperature.cpp
+++ HW 0/Temperature/Temperature.cpp
@@ -7,21 +7,22 @@
 
 #include <string>
 #include <iostream>
-using std::cout;
-using std::cin;
 using std::endl;
 using std::stod;
 using std::strtod;
 using std::string;
+using std::cout;
 
+/* function for converting F to C with the C++ function "stod" */
 double cpp_ftoc(const char* str)
 {
-    string inputF = str;
+    string inputF = str; 
     double inputFDouble = stod(inputF);
     double outputC = (((inputFDouble - 32) * 5) / 9);
     return outputC;
 }
 
+/* function for converting C to F with the C function "strtod" */
 double c_ctof(const char* str)
 {
     string inputC = str;
@@ -31,56 +32,60 @@ double c_ctof(const char* str)
     return outputF;
 }
 
+/* function for quickly checking whether a string
+    is "likely" to be an int or double */
 bool isNumber(string &s)
 {
-    for (int i = 0; i < s.length(); i++)
+    for (int i = 0; i < s.length(); i++) //iterate through all char of string
     {
-        if (isdigit(s[i]) == false)
+        if (isdigit(s[i]) == false) //if a char is not a digit
         {
-            if (s.find('.') != string::npos)
+            if (s.find('.') != string::npos) //if a "." is found, it might belong to a double
             {
                 return true;
             }
-            else return false;
+            else return false; //otherwise, this string might not be an int or double
         }
     }
 
-    return true;
+    return true; //if all char are digits, then the string is likely an int
 }
 
+/* main function with command-line arguments */
 int main(int argc, char** argv)
 {
-    string inputCommand(argv[1]);
-    string inputTemp(argv[2]);
+    string inputCommand(argv[1]); //argument for which conversion-function to use
+    string inputTemp(argv[2]); //argument for what temperature in degrees to convert
 
-    if (argc > 1)
+    if (argc > 1) //check if there's at least the name of the program
     {
-        if (argc >= 4)
+        if (argc >= 4) //tell the user to only include 2 arguments
         {
-            cout << "Please don't input more than two parameters at a time." << endl;
+            cout << "Please don't input more than two arguments at a time." << endl;
         }
-        else if (isNumber(inputTemp))
+        else if (isNumber(inputTemp)) //if everything is normal, check what conversion-function the user wants
         {
-            if (inputCommand == "--ftoc")
+            if (inputCommand == "--ftoc") //the user wants to convert F to C
             {
-                const char* inputF = argv[2];
+                const char* inputF = argv[2]; //store the user's temp input as a const char*
 
                 cout << argv[2] << " degrees Fahrenheit is " << cpp_ftoc(inputF)
-                    << " degrees Celsius!" << endl;
+                    << " degrees Celsius!" << endl; //print out the user's temp value and the conversion
             }
-            else if (inputCommand == "--ctof")
+            else if (inputCommand == "--ctof") //the user wants to convert C to F
             {
-                const char* inputC = argv[2];
+                const char* inputC = argv[2]; //store the user's temp input as a const char*
 
                 cout << argv[2] << " degrees Celsius is " << c_ctof(inputC)
-                    << " degrees Fahrenheit!" << endl;
+                    << " degrees Fahrenheit!" << endl; //print out the user's temp value and the conversion
             }
             else
             {
-                cout << "Your first parameter, 'argv[1]', wasn't understood." << endl;
+                //the user's first argument is likely to be incorrect, if the program gets to this point
+                cout << "Your first argument, 'argv[1]', wasn't understood." << endl;
             }
         }
-        else cout << "Your second parameter, 'argv[2]', wasn't understood." << endl;
+        else cout << "Your second argument, 'argv[2]', wasn't understood." << endl;
     }
 
     return 0;
