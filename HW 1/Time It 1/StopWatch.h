34f4205cf1067647ed9aa2d31b06f88fdf26176b
CS-student-account <dmisyura@alaska.edu>
2022-02-05 11:04:51 -0900

    Filled out StopWatch.h.

--- HW 1/Time It 1/StopWatch.h
+++ HW 1/Time It 1/StopWatch.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
+
+#include <string>
+using std::string;
+
+class StopWatch
+{
+private:
+
+public:
+	StopWatch(); //default constructor
+	StopWatch(const StopWatch& watch); //copy constructor
+	~StopWatch(); //destructor
+};
+#endif
