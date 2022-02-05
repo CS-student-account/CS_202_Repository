e4e0d8acb16c8126b487b8f3dab6d36d9ee182f6
CS-student-account <dmisyura@alaska.edu>
2022-02-04 23:14:21 -0900

    Created Time It 1.cpp.

---
+++ HW 1/Time It 1/Time It 1.cpp
@@ -0,0 +1,18 @@
+/*
+* Time It 1.cpp
+* David Misyura
+* 4 Feb 2022
+* HW 1 for CS 202
+*/
+
+#include <string>
+#include <iostream>
+using std::endl;
+using std::string;
+using std::cin;
+using std::cout;
+
+int main()
+{
+	return 0;
+}
