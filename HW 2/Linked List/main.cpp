f1a958b748753073b34e913d77e5b286a263ce39
CS-student-account <dmisyura@alaska.edu>
2022-02-26 18:07:07 -0900

    Added a test struct in main().

--- HW 2/Linked List/main.cpp
+++ HW 2/Linked List/main.cpp
@@ -6,10 +6,19 @@
 */
 
 #include <string>
+#include <list>
 using std::string;
 
 int main()
 {
+	struct Structure
+	{
+		int test1;
+		string test2;
+		double test3;
+		char test4;
+		float test5;
+	};
 
 	return 0;
 }
