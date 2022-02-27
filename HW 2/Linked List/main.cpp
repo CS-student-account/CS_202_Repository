a80364f1c4e2e6beacb4b10bc14341c92bac9dde
CS-student-account <dmisyura@alaska.edu>
2022-02-26 18:28:24 -0900

    Made a quick test to give values to one of the list members, traversing through the list, and displaying the values of that list.

--- HW 2/Linked List/main.cpp
+++ HW 2/Linked List/main.cpp
@@ -7,7 +7,10 @@
 
 #include <string>
 #include <list>
+#include <iostream>
 using std::string;
+using std::cout;
+using std::endl;
 
 class Structure
 {
@@ -21,15 +24,51 @@ public:
 	Structure* next;
 };
 
+void printList(Structure* n) 
+{
+	while (n != nullptr) 
+	{
+		cout << n->test1 << " ";
+		n = n->next;
+	}
+	cout << endl;
+}
+
 int main()
 {
 	Structure* first = nullptr;
 	Structure* second = nullptr;
 	Structure* third = nullptr;
+	Structure* fourth = nullptr;
+	Structure* fifth = nullptr;
+	Structure* sixth = nullptr;
 
 	first = new Structure();
 	second = new Structure();
 	third = new Structure();
+	fourth = new Structure();
+	fifth = new Structure();
+	sixth = new Structure();
+
+	first->test1 = 1;
+	first->next = second;
+
+	second->test1 = 2;
+	second->next = third;
+
+	third->test1 = 3;
+	third->next = fourth;
+
+	fourth->test1 = 4;
+	fourth->next = fifth;
+
+	fifth->test1 = 5;
+	fifth->next = sixth;
+
+	sixth->test1 = 6;
+	sixth->next = nullptr;
+
+	printList(first);
 
 	return 0;
 }
