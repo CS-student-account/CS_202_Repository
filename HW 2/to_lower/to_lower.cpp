58652c3a59e2dcbbfd521e3a6e30ac13798a41ff
CS-student-account <dmisyura@alaska.edu>
2022-02-27 18:54:22 -0900

    Created to_lower.cpp.

---
+++ HW 2/to_lower/to_lower.cpp
@@ -0,0 +1,17 @@
+/*
+* to_lower.cpp
+* David Misyura
+* 27 Feb 2022
+* HW 2 for CS 202
+*/
+
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    return 0;
+}
