09686712b8f4383447e887179da589b154291edd
CS-student-account <dmisyura@alaska.edu>
2022-02-27 18:51:29 -0900

    Finished Memory Layout.cpp.

--- HW 2/Memory Layout/Memory Layout.cpp
+++ HW 2/Memory Layout/Memory Layout.cpp
@@ -6,18 +6,55 @@
 */
 
 #include <string>
-#include <list>
 #include <iostream>
-#include <random>
 using std::string;
 using std::cout;
 using std::endl;
-using std::random_device;
-using std::mt19937;
-using std::uniform_int_distribution;
-using std::swap;
+
+int staticData;
 
 int main()
 {
+    const int codeData = 1;
 
+    int* freeMemory = nullptr;
+    freeMemory = new int;
+
+    int stackData;
+
+    if (&staticData > &stackData)
+    {
+        cout << "Static data is in higher memory than the Stack." << endl;
+
+        if (&staticData > &codeData)
+        {
+            cout << "Static data is in higher memory than Code data." << endl;
+        }
+
+        if (&staticData > freeMemory)
+        {
+            cout << "Static data is in higher memory than the free memory." << endl;
+        }
+    }
+
+    if (&stackData > &codeData)
+    {
+        cout << "Stack is in higher memory than the Code data." << endl;
+
+        if (&stackData < freeMemory)
+        {
+            cout << "Stack is in lower memory than the free memory." << endl;
+        }
+    }
+
+    if (&codeData < freeMemory)
+    {
+        cout << "Code data is in lower memory than the free memory." << endl;
+    }
+
+    cout << endl << "Thus, the memory layout of this program is: " << endl
+        << "static data > " << "free memory > " << "stack > " << "code data" << endl;
+
+    delete freeMemory;
+    return 0;
 }
