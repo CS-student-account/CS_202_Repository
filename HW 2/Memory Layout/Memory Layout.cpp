cc4c73da251f3b8898c600f86c0327789993a587
CS-student-account <dmisyura@alaska.edu>
2022-02-27 17:43:51 -0900

    Created Memory Layout.cpp.

---
+++ HW 2/Memory Layout/Memory Layout.cpp
@@ -0,0 +1,23 @@
+/*
+* Memory List.cpp
+* David Misyura
+* 27 Feb 2022
+* HW 2 for CS 202
+*/
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <random>
+using std::string;
+using std::cout;
+using std::endl;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::swap;
+
+int main()
+{
+
+}
