5fbad4d564069405715a1b4c4ff6446f8e1c5f56
CS-student-account <dmisyura@alaska.edu>
2022-02-27 19:29:38 -0900

    Finished to_lower() function.

--- HW 2/to_lower/to_lower.cpp
+++ HW 2/to_lower/to_lower.cpp
@@ -5,20 +5,26 @@
 * HW 2 for CS 202
 */
 
-#include <string>
 #include <iostream>
-using std::string;
 using std::cout;
 using std::endl;
 
 void to_lower(char* s)
 {
-
+    for (; *s != '\0'; ++s) //iterate until 0 is reached at end of C-string
+    {
+        *s |= (1 << 5); //sets the fifth bit of the char to 1
+                        //using bitwise operator
+    }
 }
 
 int main()
 {
-    char string[] = "Hello, World!0";
+    char string[] = { 'H', 'E', 'L', 'L', 'O', ',', ' ', 'W', '0', 'R', 'L', 'D', '!', '\0'};
+    cout << string << endl;
+
+    to_lower(string);
+    cout << string << endl;
 
     return 0;
 }
