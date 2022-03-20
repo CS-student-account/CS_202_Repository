74434e47a61534d19b97be69c29f3f35d779d2c9
CS-student-account <dmisyura@alaska.edu>
2022-03-19 23:20:53 -0800

    Used iomanip to standardize formatting of output;

--- HW 3/A Simple Tokenizer/tokenizer.cpp
+++ HW 3/A Simple Tokenizer/tokenizer.cpp
@@ -31,85 +31,10 @@ using std::quoted;
 using std::map;
 using std::sort;
 using std::pow;
-
-bool readLine(string &str)
-{
-	cout << "Please type in some text: ";
-	if (getline(cin, str)) //accept a string input from the user and save it as the string str
-	{
-		return true; //if a string is accepted, return true
-	}
-	else return false; //if no string is accepted, return false
-}
-
-unsigned stringToTokenWS(const string &input, vector<string> &tokens)
-{
-	istringstream instream(input); //reads the string passed in through istringstream
-	string token;
-
-	while (instream >> token) //pushes each of the tokens from the string separated by a space into the vector of strings
-	{
-		tokens.push_back(token);
-	}
-
-	tokens.push_back(" "); //at the end of each line, push a whitespace
-	return tokens.size(); //returns how many tokens there are
-}
-
-void analyzeTokens(const vector<string> &tokens)
-{
-	vector<string> final; //a tempoorary vector for pushing in final printouts
-
-	for (const auto &i : tokens) //iterate through each string in the vector
-	{
-		for (const int &character : i) //iterate through each character of each string
-		{
-			if (character <= 122 && character >= 97)
-			{
-				final.push_back("[identifier] \"" + i + "\"" + ' ');
-				break;
-			}
-
-			if (character <= 57 && character >= 48)
-			{
-				final.push_back("[integer] \"" + i + "\"" + ' ');
-				break;
-			}
-
-			if (character == 34)
-			{
-				final.push_back("[quote] " + i + ' ');
-				break;
-			}
-
-			if (character >= 33 && character <= 47)
-			{
-				final.push_back("[other] \"" + i + "\"" + ' ');
-				break;
-			}
-
-			if (character >= 58 && character <= 64)
-			{
-				final.push_back("[other] \"" + i + "\"" + ' ');
-				break;
-			}
-
-			if (character == 32)
-			{
-				final.push_back("[whitespace] \"\"");
-				break;
-			}
-		}
-	}
-
-	if (tokens[tokens.size() - 2] == "end") //if "end" is input, this program will print out the identified strings
-	{
-		for (int j = 0; j < final.size() - 2; j++)
-		{
-			cout << endl << final[j] << endl;
-		}
-	}
-}
+using std::setw;
+using std::left;
+using std::right;
+using std::setfill;
 
 bool checkFile(const string& filename)
 {
@@ -145,37 +70,6 @@ bool checkFile(const string& filename)
 	}
 }
 
-string excerpt(const string &filename)
-{
-	ifstream fin(filename);
-
-	int total_lines = 0; //total number of lines in book
-	vector<string> lines; //vector of strings to place words
-
-	while (true)
-	{
-		string line;
-		getline(fin, line);
-		total_lines++;
-		lines.push_back(line);
-
-		if (!fin)
-		{
-			if (fin.eof())
-			{
-				cout << "excerptinator has completed." << endl << endl;
-				return lines[150]; //return a random line of text
-			}
-			else
-			{
-				return "Error during transmit.";
-			}
-			break;
-		}
-	}
-}
-
-
 vector<string> lineToTokens(const string &line)
 {
 	vector<string> tokenVector; //vector of string to save tokens
@@ -186,7 +80,10 @@ vector<string> lineToTokens(const string &line)
 
 	while (getline(tokenStream, token, ' ')) //getline with space as delimiter
 	{
-		tokenVector.push_back(token);
+		if (!token.empty())
+		{
+			tokenVector.push_back(token);
+		}
 	}
 
 	return tokenVector; 
@@ -255,6 +152,6 @@ void printTokens(ostream& os, const vector<TokenAndPosition>& tokens)
 {
 	for (TokenAndPosition i : tokens)
 	{
-		os << "[Line " << i._line << ", Column " << i._column << "]: " << quoted(i._token) << endl;
+		os << "[Line " << right << setfill(' ') << setw(4) << i._line << setw(6) << ", Column " << left << setfill(' ') << setw(2) << i._column << "]: " << quoted(i._token) << endl;
 	}
 }
