f670042c546ee012dc91940584668c656f125a9b
CS-student-account <dmisyura@alaska.edu>
2022-03-21 13:07:42 -0800

    Fixed Money constructors.

--- HW 3/Money Class/Money.cpp
+++ HW 3/Money Class/Money.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
+#include <typeinfo>
 using std::cout;
 using std::endl;
 using std::string;
@@ -21,3 +22,67 @@ using std::left;
 using std::right;
 using std::setfill;
 using std::setprecision;
+using std::stoi;
+using std::ostream;
+
+Money::Money() : money(0)
+{
+
+}
+
+Money::Money(const int &amount) : money(amount)
+{
+
+}
+
+Money::Money(const float &amount) : money(amount)
+{
+
+}
+
+Money::~Money()
+{
+
+}
+
+ostream& operator<<(ostream& out, const Money& amount)
+{
+	float temp = amount.money;
+
+	out << "$";
+
+	if (amount.money > 0)
+	{
+		out << amount.money;
+	}
+	else if (amount.money < 0)
+	{
+		out << amount.money;
+	}
+
+	return out;
+}
+
+Money& Money::operator+=(const Money& rhs)
+{
+	money += rhs.money;
+
+	return *this;
+}
+
+Money Money::operator+(const Money& rhs)
+{
+	Money out = *this;
+
+	if (out.money > 0)
+	{
+		out.money += rhs.money;
+	}
+
+	if (out.money > 0)
+	{
+		out.money += rhs.money;
+	}
+
+	return out;
+}
--- HW 3/Money Class/Money.h
+++ HW 3/Money Class/Money.h
@@ -4,12 +4,20 @@
 #include <string>
 #include <vector>
 using std::string;
+using std::ostream;
 
 class Money
 {
 private:
+	float money;
 	
 public:
-	
+	Money();
+	Money(const int &amount);
+	Money(const float &amount);
+	~Money();
+	friend ostream& operator<<(ostream& out, const Money& amount);
+	Money& operator+=(const Money& rhs);
+	Money operator+(const Money& rhs);
 };
 #endif 
