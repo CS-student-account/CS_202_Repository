d4c027bea436359ac1b7e8cca0ad51086d5cc966
CS-student-account <dmisyura@alaska.edu>
2022-03-21 11:42:31 -0800

    Created Money Class project, and added catch.hpp, Money.h, Money.cpp, and moneytest.cpp.

---
+++ HW 3/Money Class/Money.cpp
@@ -0,0 +1,23 @@
+/*
+* Money.cpp
+* David Misyura
+* 20 Mar 2022
+* Money.cpp for CS 202
+*/
+
+#include "Money.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::getline;
+using std::setw;
+using std::left;
+using std::right;
+using std::setfill;
+using std::setprecision;
---
+++ HW 3/Money Class/Money.h
@@ -0,0 +1,15 @@
+#ifndef MONEY_H
+#define MONEY_H
+
+#include <string>
+#include <vector>
+using std::string;
+
+class Money
+{
+private:
+	
+public:
+	
+};
+#endif 
