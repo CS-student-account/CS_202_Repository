ed7a5c3b42517a40b43f37482656d2d8af43c653
CS-student-account <dmisyura@alaska.edu>
2022-03-22 01:44:34 -0800

    Removed unneeded headers, and gave comments to code.

--- HW 3/Money Class/Money.cpp
+++ HW 3/Money Class/Money.cpp
@@ -7,29 +7,17 @@
 
 #include "Money.h"
 #include <iostream>
-#include <string>
-#include <vector>
-#include <sstream>
 #include <iomanip>
-#include <typeinfo>
 #include <cmath>
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-using std::getline;
 using std::setw;
-using std::left;
 using std::right;
 using std::setfill;
 using std::setprecision;
-using std::stoi;
 using std::ostream;
 using std::abs;
-using std::round;
-using std::fmod;
 using std::remainder;
 
+/* Constructors and Destructor */
 Money::Money() : money(0.00)
 {
 
@@ -50,135 +38,136 @@ Money::~Money()
 
 }
 
-ostream& operator<<(ostream& out, const Money& amount)
+/* Ostream Operator */
+ostream &operator<<(ostream &out, const Money &amount)
 {
-	double temp = amount.money;
-	int dollars = round(temp);
-	double cents = remainder(temp,100);
+	//gives dollars and cents if double is given, otherwise performs modulus to give cents
+	double cash = remainder(amount.money, 100);
 
 	if (amount.money == 0)
 	{
-		out << "$" << 0 << '.' << left << setfill('0') << setw(2) << 0;
+		out << "$0.00";
 	}
-	else if (amount.money > 0)
+	else if (amount.money > 0) //positive amount
 	{
-		if (static_cast<int>(amount.money) == amount.money) 
+		if (static_cast<int>(amount.money) == amount.money) //if int, which are just cents
 		{
-			out << "$" << 0 << '.' << right << setfill('0') << setw(2) << cents;
+			out << "$0" << '.' << right << setfill('0') <<setw(2) << cash;
 		}
-		else 
+		else //otherwise, dollars and cents are given
 		{
-			//'.' << right << setfill('0') << setw(2) << temp
-			out << "$" << right << setfill('0') << setw(4) << setprecision(3) << cents;
+			out << "$" << right << setfill('0') << setprecision(3) << cash;
 		}
 	}
-	else
+	else //else if negative amount
 	{
-		if (static_cast<int>(amount.money) == amount.money)
+		if (static_cast<int>(amount.money) == amount.money) //if int, which are just cents
 		{
-			out << "-$" << 0 << '.' << right << setfill('0') << setw(2) << abs(cents);
+			out << "-$0" << '.' << right << setfill('0') << setw(2) << abs(cash);
 		}
-		else
+		else //otherwise, dollars and cents are given
 		{
-			//'.' << right << setfill('0') << setw(2) << temp
-			out << "-$" << right << setfill('0') << setw(4) << setprecision(3) << abs(cents);
+			out << "-$" << right << setfill('0') << setprecision(3) << abs(cash);
 		}
 	}
 
 	return out;
 }
 
-bool operator==(const Money &lhs, const Money& rhs)
+/* Equality Operators */
+bool operator==(const Money &lhs, const Money &rhs)
 {
 	return (lhs.money == rhs.money);
 }
 
-bool operator!=(const Money& lhs, const Money& rhs)
+bool operator!=(const Money &lhs, const Money &rhs)
 {
 	return !(lhs.money == rhs.money);
 }
 
-bool operator<(const Money& lhs, const Money& rhs)
+bool operator<(const Money &lhs, const Money &rhs)
 {
 	return (lhs.money < rhs.money);
 }
 
-bool operator<=(const Money& lhs, const Money& rhs)
+bool operator<=(const Money &lhs, const Money &rhs)
 {
 	return (lhs.money <= rhs.money);
 }
 
-bool operator>(const Money& lhs, const Money& rhs)
+bool operator>(const Money &lhs, const Money &rhs)
 {
 	return (lhs.money > rhs.money);
 }
 
-bool operator>=(const Money& lhs, const Money& rhs)
+bool operator>=(const Money &lhs, const Money &rhs)
 {
 	return (lhs.money >= rhs.money);
 }
 
-Money& Money::operator+=(const Money& rhs)
+/* Relational Operators */
+Money &Money::operator+=(const Money &rhs)
 {
 	money += rhs.money;
 
 	return *this;
 }
 
-Money& Money::operator-=(const Money& rhs)
+Money &Money::operator-=(const Money &rhs)
 {
 	money -= rhs.money;
 
 	return *this;
 }
 
-Money& Money::operator*=(const double& rhs)
+Money &Money::operator*=(const double &rhs)
 {
 	money *= rhs;
 
 	return *this;
 }
 
-Money& Money::operator*=(const Money& rhs)
+Money &Money::operator*=(const Money &rhs)
 {
 	money *= rhs.money;
 
 	return *this;
 }
 
-Money& Money::operator/=(const double& rhs)
+Money &Money::operator/=(const double &rhs)
 {
 	money /= rhs;
 
 	return *this;
 }
 
-Money operator+(const Money& lhs, const Money& rhs)
+/* Arithmetic Operators */
+Money operator+(const Money &lhs, const Money &rhs)
 {
 	return Money(lhs.money + rhs.money);
 }
 
-Money operator-(const Money& lhs, const Money& rhs)
+Money operator-(const Money &lhs, const Money &rhs)
 {
 	return Money(lhs.money - rhs.money);
 }
 
-Money operator*(const double& lhs, const Money& rhs)
+Money operator*(const double &lhs, const Money &rhs)
 {
 	return Money(lhs * rhs.money);
 }
 
-Money operator*(const Money& lhs, const double& rhs)
+Money operator*(const Money &lhs, const double &rhs)
 {
 	return Money(lhs.money * rhs);
 }
 
-Money operator/(const Money& lhs, const double& rhs)
+Money operator/(const Money &lhs, const double &rhs)
 {
 	return Money(lhs.money / rhs);
 }
 
-Money operator/(const Money& lhs, const Money& rhs)
+Money operator/(const Money &lhs, const Money &rhs)
 {
 	return Money(lhs.money / rhs.money);
 }
--- HW 3/Money Class/Money.h
+++ HW 3/Money Class/Money.h
@@ -1,9 +1,7 @@
 #ifndef MONEY_H
 #define MONEY_H
 
-#include <string>
-#include <vector>
-using std::string;
+#include <iostream>
 using std::ostream;
 
 class Money
@@ -13,29 +11,30 @@ private:
 	
 public:
 	Money();
-	Money(const int &amount);
-	Money(const double &amount);
+	Money(const int &amount); //just cents
+	Money(const double &amount); //dollars and cents
 	~Money();
-	friend ostream& operator<<(ostream& out, const Money& amount);
 
-	friend bool operator==(const Money& lhs, const Money& rhs);
-	friend bool operator!=(const Money& lhs, const Money& rhs);
-	friend bool operator<(const Money& lhs, const Money& rhs);
-	friend bool operator<=(const Money& lhs, const Money& rhs);
-	friend bool operator>(const Money& lhs, const Money& rhs);
-	friend bool operator>=(const Money& lhs, const Money& rhs);
+	friend ostream &operator<<(ostream &out, const Money &amount);
 
-	Money& operator+=(const Money& rhs);
-	Money& operator-=(const Money& rhs);
-	Money& operator*=(const double& rhs);
-	Money& operator*=(const Money& rhs);
-	Money& operator/=(const double& rhs);
+	friend bool operator==(const Money &lhs, const Money &rhs);
+	friend bool operator!=(const Money &lhs, const Money &rhs);
+	friend bool operator<(const Money &lhs, const Money &rhs);
+	friend bool operator<=(const Money &lhs, const Money &rhs);
+	friend bool operator>(const Money &lhs, const Money &rhs);
+	friend bool operator>=(const Money &lhs, const Money &rhs);
 
-	friend Money operator+(const Money& lhs, const Money& rhs);
-	friend Money operator-(const Money& lhs, const Money& rhs);
-	friend Money operator*(const double& lhs, const Money& rhs);
-	friend Money operator*(const Money& lhs, const double& rhs);
-	friend Money operator/(const Money& lhs, const double& rhs);
-	friend Money operator/(const Money& lhs, const Money& rhs);
+	Money &operator+=(const Money &rhs);
+	Money &operator-=(const Money &rhs);
+	Money &operator*=(const double &rhs);
+	Money &operator*=(const Money &rhs);
+	Money &operator/=(const double &rhs);
+
+	friend Money operator+(const Money &lhs, const Money &rhs);
+	friend Money operator-(const Money &lhs, const Money &rhs);
+	friend Money operator*(const double &lhs, const Money &rhs);
+	friend Money operator*(const Money &lhs, const double &rhs);
+	friend Money operator/(const Money &lhs, const double &rhs);
+	friend Money operator/(const Money &lhs, const Money &rhs);
 };
 #endif 
