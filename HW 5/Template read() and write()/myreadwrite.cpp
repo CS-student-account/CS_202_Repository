c4f7d601883f9fd110d205ac40b1a851a6fb9e51
CS-student-account <dmisyura@alaska.edu>
2022-04-26 00:31:46 -0800

    Created myreadwrite.cpp and myreadwrite.h.

---
+++ HW 5/Template read() and write()/myreadwrite.cpp
@@ -0,0 +1,12 @@
+/*
+* myreadwrite.cpp
+* David Misyura
+* 25 Apr 2022
+* HW 5 for CS 202
+*/
+
+#include "myreadwrite.h"
+#include <iostream>
+#include <string>
+using std::string;
+using std::ostream;
---
+++ HW 5/Template read() and write()/myreadwrite.h
@@ -0,0 +1,25 @@
+/*
+* myreadwrite.h
+* David Misyura
+* 25 Apr 2022
+* HW 5 for CS 202
+*/
+
+#ifndef MYREADWRITE_H
+#define MYREADWRITE_H
+
+#include <iostream>
+#include <string>
+using std::string;
+using std::ostream;
+
+class Box
+{
+private:
+
+
+public:
+	
+};
+
+#endif
